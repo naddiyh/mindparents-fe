f10487820ddb4706d8ad6fc20997289bfbc2e407
von-ai <kuhhakuzuhdi@gmail.com>
2024-05-23 10:41:12 +0800

    refactor 5 fitur terkait fixed

--- components/FiturTerkait/Fitur.tsx
+++ components/FiturTerkait/Fitur.tsx
@@ -5,16 +5,15 @@ import { features, Feature } from "./FiturObject";
 const Fitur = () => {
   return(
     <>
-      <main className="px-8 md:px-6 lg:px-24">
+      <main className="px-6 lg:px-24">
         <section className="flex justify-center pt-5">
-          <p className="text-heading-m text-primary-purple font-bold">Fitur</p>
-          <p className="text-heading-m font-bold px-2">Terkait</p>
+          <p className="text-heading-s lg:text-heading-m text-black font-bold">Fitur Terkait</p>
         </section>
         <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 pt-10">
           {features.map((feature: Feature, index: number) => (
             <div 
               key={index} 
-              className="flex flex-col sm:flex-row gap-3 items-center sm:items-start"
+              className="flex flex-col sm:flex-row gap-3 items-center sm:items-start sm:gap-y-4"
             >
               <div className="w-full h-auto sm:w-[70px] sm:h-[50px] flex-shrink-0">
                 <Image 
