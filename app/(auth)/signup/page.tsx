ee4979944d8409dcca16f5ab529fd4f26f0c1711
naddiyh <nadtss12@gmail.com>
2024-05-30 23:30:54 +0800

    forgot password

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "@/components/Navbar";
 
 export default function SignupPage() {
   return (
-    <main>
+    <main className="flex min-h-screen w-screen flex-col items-center justify-center py-32">
       <Navbar />
       <SignUp />
     </main>
