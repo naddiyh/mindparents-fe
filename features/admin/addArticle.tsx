c2d6fed5e156b716e925f761497f42ebf1d6d0d7
BryanJericho <bryanpanggalo@gmail.com>
2024-06-07 21:29:01 +0800

    crud finishing

--- features/admin/addArticle.tsx
+++ features/admin/addArticle.tsx
@@ -12,10 +12,11 @@ const AdminPage: React.FC = () => {
   const [title, setTitle] = useState('');
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
   const [image, setImage] = useState<File | null>(null);
-  const [imageUrl, setImageUrl] = useState('');
+  const [img, setImg] = useState('');
   const [video, setVideo] = useState<File | null>(null);
   const [videoUrl, setVideoUrl] = useState('');
   const [path, setPath] = useState('');
+  const [creatorName, setCreatorName] = useState('');
 
   useEffect(() => {
     fetchArticles();
@@ -34,7 +35,6 @@ const AdminPage: React.FC = () => {
       setVideo(videoFile);
     }
   };
-  
 
   const handleUpload = (file: File | null, type: 'image' | 'video') => {
     if (file) {
@@ -51,17 +51,24 @@ const AdminPage: React.FC = () => {
           console.log(error); // Handle errors
         },
         () => {
-          getDownloadURL(uploadTask.snapshot.ref)
-            .then((downloadURL) => {
-              if (type === 'image') {
-                setImageUrl(downloadURL);
-                alert('Image uploaded successfully!');
-              } else {
+          if (type === 'image') {
+            setImg(storagePath);
+            getDownloadURL(uploadTask.snapshot.ref) // Mendapatkan URL unduhan gambar
+              .then((downloadURL) => {
+                // Set URL unduhan gambar ke state atau melakukan operasi lain yang diperlukan
+                console.log('Image download URL:', downloadURL);
+                // Disini Anda bisa melakukan apapun dengan URL unduhan gambar, seperti menyimpannya ke state atau ke database
+              })
+              .catch((error) => console.error('Error getting download URL:', error));
+            alert('Image uploaded successfully!');
+          } else {
+            getDownloadURL(uploadTask.snapshot.ref)
+              .then((downloadURL) => {
                 setVideoUrl(downloadURL);
                 alert('Video uploaded successfully!');
-              }
-            })
-            .catch((error) => console.error('Error getting download URL:', error));
+              })
+              .catch((error) => console.error('Error getting download URL:', error));
+          }
         }
       );
     } else {
@@ -76,8 +83,9 @@ const AdminPage: React.FC = () => {
         const articleData = {
           title,
           content: convertToRaw(editorState.getCurrentContent()),
-          imageUrl,
+          img,
           videoUrl,
+          creatorName, // Include creatorName in the article data
           createdAt: serverTimestamp()
         };
 
@@ -85,8 +93,9 @@ const AdminPage: React.FC = () => {
 
         setTitle('');
         setEditorState(EditorState.createEmpty());
-        setImageUrl('');
+        setImg('');
         setVideoUrl('');
+        setCreatorName('');
         alert('Artikel berhasil ditambahkan!');
         fetchArticles();
       } catch (error) {
@@ -125,13 +134,15 @@ const AdminPage: React.FC = () => {
       await updateDoc(doc(db, `articles/${path}`, articleId), {
         title,
         content: convertToRaw(editorState.getCurrentContent()),
-        imageUrl,
+        img,
         videoUrl,
+        creatorName,
       });
       setTitle('');
       setEditorState(EditorState.createEmpty());
-      setImageUrl('');
+      setImg('');
       setVideoUrl('');
+      setCreatorName('');
       alert('Artikel berhasil diperbarui!');
       fetchArticles();
     } catch (error) {
@@ -177,89 +188,105 @@ const AdminPage: React.FC = () => {
             type="file"
             onChange={handleImageChange}
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-          />
-          <button
-            type="button"
-            onClick={() => handleUpload(image, 'image')}
-            className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md"
-          >
-            Upload Gambar
-          </button>
-        </div>
-        <div className="mb-2">
-          <label className="block text-sm font-medium text-gray-700">Video</label>
-          <input
-            type="file"
-            onChange={handleVideoChange}
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-          />
-          <button
-            type="button"
-            onClick={() => handleUpload(video, 'video')}
-            className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md"
-          >
-            Upload Video
-          </button>
-        </div>
-        <div className="mb-2">
-          <label className="block text-sm font-medium text-gray-700">Path</label>
-          <select
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-            onChange={(e) => setPath(e.target.value)}
-          >
-            <option value="">Pilih Path</option>
-            <option value="kehamilan/trisemester-1">Kehamilan - Trisemester 1</option>
-            <option value="kehamilan/trisemester-2">Kehamilan - Trisemester 2</option>
-            <option value="kehamilan/trisemester-3">Kehamilan - Trisemester 3</option>
-            <option value="perkembangan-anak/2-6tahun">Perkembangan Anak - 2-6 Tahun</option>
-            <option value="perkembangan-anak/2-18tahun">Perkembangan Anak - 2-18 Tahun</option>
-            <option value="perkembangan-anak/6-12tahun">Perkembangan Anak - 6-12 Tahun</option>
-          </select>
-        </div>
-        <button
-          type="submit"
-          className="mt-4 px-4 py-2 bg-green-500 text-white rounded-md"
-        >
-          Tambahkan Artikel
-        </button>
-      </form>
-      
-      <h2 className="text-xl font-bold mb-4">Daftar Artikel</h2>
-      <ul>
-        {articles.map((article) => (
-          <li key={article.id} className="mb-4 p-4 border rounded-md">
-            <h3 className="text-lg font-semibold">{article.title}</h3>
-            <p>{convertDraftToPlainText(JSON.stringify(article.content))}</p>
-            {article.imageUrl && (
-              <img src={article.imageUrl} alt="Article" className="mt-2 w-full h-auto" />
-            )}
-            {article.videoUrl && (
-              <video controls className="mt-2 w-full h-auto">
-                <source src={article.videoUrl} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-            )}
+            />
             <button
-              onClick={() => handleDelete(article.id)}
-              className="mt-2 px-4 py-2 bg-red-500 text-white rounded-md"
+              type="button"
+              onClick={() => handleUpload(image, 'image')}
+              className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md"
             >
-              Hapus
+              Upload Gambar
             </button>
+          </div>
+          <div className="mb-2">
+            <label className="block text-sm font-medium text-gray-700">Video</label>
+            <input
+              type="file"
+              onChange={handleVideoChange}
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            />
             <button
-              onClick={() => {
-                setTitle(article.title);
-                setEditorState(EditorState.createWithContent(convertFromRaw(JSON.parse(article.content))));
-                handleUpdate(article.id);
-              }}
-              className="mt-2 ml-2 px-4 py-2 bg-yellow-500 text-white rounded-md"
+              type="button"
+              onClick={() => handleUpload(video, 'video')}
+              className="mt-2 px-4 py-2 bg-blue-500 text-white rounded-md"
             >
-              Perbarui
+              Upload Video
             </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default AdminPage;
+          </div>
+          <div className="mb-2">
+            <label className="block text-sm font-medium text-gray-700">Nama Kreator</label>
+            <input
+              type="text"
+              value={creatorName}
+              onChange={(e) => setCreatorName(e.target.value)}
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div className="mb-2">
+            <label className="block text-sm font-medium text-gray-700">Path</label>
+            <select
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              onChange={(e) => setPath(e.target.value)}
+            >
+              <option value="">Pilih Path</option>
+              <option value="kehamilan/trisemester-1">Kehamilan - Trisemester 1</option>
+              <option value="kehamilan/trisemester-2">Kehamilan - Trisemester 2</option>
+              <option value="kehamilan/trisemester-3">Kehamilan - Trisemester 3</option>
+              <option value="perkembangan-anak/2-6tahun">Perkembangan Anak - 2-6 Tahun</option>
+              <option value="perkembangan-anak/2-18tahun">Perkembangan Anak - 2-18 Tahun</option>
+              <option value="perkembangan-anak/6-12tahun">Perkembangan Anak - 6-12 Tahun</option>
+              <option value="persiapan-ortu/persiapan-hubungan">Persiapan Ortu - Persiapan Hubungan</option>
+              <option value="persiapan-ortu/persiapan-intelektual">Persiapan Ortu - Persiapan Intelektual</option>
+              <option value="persiapan-ortu/persiapan-mental">Persiapan Ortu - Persiapan Mental</option>
+            </select>
+          </div>
+          <button
+            type="submit"
+            className="mt-4 px-4 py-2 bg-green-500 text-white rounded-md"
+          >
+            Tambahkan Artikel
+          </button>
+        </form>
+        
+        <h2 className="text-xl font-bold mb-4">Daftar Artikel</h2>
+        <ul>
+          {articles.map((article) => (
+            <li key={article.id} className="mb-4 p-4 border rounded-md">
+              <h3 className="text-lg font-semibold">{article.title}</h3>
+              <p>{convertDraftToPlainText(JSON.stringify(article.content))}</p>
+              {article.img && (
+                <img src={`/${article.img}`} alt="Article" className="mt-2 w-full h-auto" />
+              )}
+              {article.videoUrl && (
+                <video controls className="mt-2 w-full h-auto">
+                  <source src={article.videoUrl} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              )}
+              <button
+                onClick={() => handleDelete(article.id)}
+                className="mt-2 px-4 py-2 bg-red-500 text-white rounded-md"
+              >
+                Hapus
+              </button>
+              <button
+                onClick={() => {
+                  setTitle(article.title);
+                  setEditorState(EditorState.createWithContent(convertFromRaw(JSON.parse(article.content))));
+                  setImg(article.img);
+                  setVideoUrl(article.videoUrl);
+                  setCreatorName(article.creatorName); // Set nama kreator pada form update
+                  handleUpdate(article.id);
+                }}
+                className="mt-2 ml-2 px-4 py-2 bg-yellow-500 text-white rounded-md"
+              >
+                Perbarui
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default AdminPage;
