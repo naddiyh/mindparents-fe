b600773d9757e8d4925b730a99546284f952e764
BryanJericho <bryanpanggalo@gmail.com>
2024-06-07 21:29:01 +0800

    crud finishing

--- config/firebase.ts
+++ config/firebase.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage"; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyDUwoOY7_gc-z_4aRlhwrdsKVqVQLw8y8k",
@@ -20,5 +21,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const storage = getStorage(app); 
 
-export { auth, db, provider };
+export { auth, db, provider, storage }; 
