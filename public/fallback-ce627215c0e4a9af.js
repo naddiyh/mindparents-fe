6e53366d1651099dc8facde66614193979e7d2cf
naddiyh <nadtss12@gmail.com>
2024-06-27 22:35:41 +0800

    tes build

--- public/fallback-ce627215c0e4a9af.js
+++ public/fallback-ce627215c0e4a9af.js
@@ -1,17 +1,1 @@
-self.fallback = async (a) => {
-  let { destination: e, url: c } = a,
-    l = {
-      document: "/~offline",
-      image: "/fallback.webp",
-      audio: "/fallback.mp3",
-      video: "/fallback.mp4",
-      font: "/fallback-font.woff2",
-    }[e];
-  return l
-    ? caches.match(l, { ignoreSearch: !0 })
-    : "" === e && c.match(/\/_next\/data\/.+\/.+\.json$/i)
-      ? caches.match("/_next/data/mt1ghjZ2fwq6ePIosy87e/fallback.json", {
-          ignoreSearch: !0,
-        })
-      : Response.error();
-};
+self.fallback=async a=>{let{destination:e,url:c}=a,l={document:"/~offline",image:"/fallback.webp",audio:"/fallback.mp3",video:"/fallback.mp4",font:"/fallback-font.woff2"}[e];return l?caches.match(l,{ignoreSearch:!0}):""===e&&c.match(/\/_next\/data\/.+\/.+\.json$/i)?caches.match("/_next/data/gRiOEXwSbGv3tEsOKZ_46/fallback.json",{ignoreSearch:!0}):Response.error()};
